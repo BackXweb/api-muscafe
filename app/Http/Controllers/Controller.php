a032e42fe6d1a868ba83e6395d20e63ca02b0da2
Fedorov Nikita <fedorov.nukuta@gmail.com>
2023-05-21 13:34:02 +0300

    Add response in json Add custom response statuses in json Add test action /test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,33 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use function PHPUnit\Framework\returnArgument;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $storage = 'https://api.cvan.ru/storage/';
+
+    protected function getDomain() {
+        return ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
+    }
+
+    protected function outputData($message, $data = []) {
+        $data = collect($data);
+
+        if ($data->isNotEmpty() && $data->count() > 0)
+            return response()->json(['message' => $message['with_data'], 'data' => $data], 200);
+        else
+            return response()->json(['message' => $message['without_data'], 'data' => $data], 200);
+    }
+
+    protected function outputError($message, $code) {
+        return response()->json(['message' => $message], $code);
+    }
+
+    protected function getFilename($path) {
+        $filename = explode('/', $path);
+        return array_pop($filename);
+    }
 }
