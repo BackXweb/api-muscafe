b42a9f2d4bbb47e36cc734a113d9cb1a24ace39a
Fedorov Nikita <fedorov.nukuta@gmail.com>
2023-06-01 15:47:39 +0300

    Fix filters users for role_id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -117,6 +117,9 @@ class UserController extends Controller
     {
         $query = User::whereHas('role', function ($query) {
             $query->where('name', 'LIKE', request('role', 'user.%'));
+
+            if (request('role', 0))
+                $query->where('id', request('role', 0));
         })->with('manager', function ($query) {
             $query->select(['id', 'name']);
         })->with('role', function ($query) {
