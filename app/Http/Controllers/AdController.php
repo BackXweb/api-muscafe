ad7b1005991365e34735d04d8a62119462516b06
Fedorov Nikita <fedorov.nukuta@gmail.com>
2023-06-20 14:35:01 +0300

    Change storage ads in server

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -34,7 +34,7 @@ class AdController extends Controller
 
     public function store(StoreRequest $request) {
         $validated = $request->validated();
-        $validated['storage'] = Storage::putFile('ads/' . $request->user()->id, $request->file('file'));
+        $validated['storage'] = Storage::putFile('/ads/' . $request->user()->id, $request->file('file'), 'public');
         $validated['user_id'] = $request->user()->id;
 
         if ($validated['storage'] !== false) {
