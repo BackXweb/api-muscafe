913ba81fb8670e7e1ddcc27e506e986d5607588b
Fedorov Nikita <fedorov.nukuta@gmail.com>
2023-07-04 17:55:29 +0300

    Fix get statistic data from DB

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Carbon;
 class StatisticController extends Controller
 {
     public function index(Request $request) {
-        $statistics = Statistic::where('facility_id', $request->id)->whereDate('created_at', '<=', date('Y-m-d H:i:s', strtotime(request('date_start', Carbon::today()))))->whereDate('created_at', '>=', date('Y-m-d H:i:s', strtotime(request('date_end', Carbon::yesterday()))));
+        $statistics = Statistic::where('facility_id', $request->id)->whereDate('created_at', '>=', date('Y-m-d H:i:s', strtotime(request('date_start', Carbon::today()))))->whereDate('created_at', '<=', date('Y-m-d H:i:s', strtotime(request('date_end', Carbon::tomorrow()))));
 
         if (!empty($request->is_ad) || $request->is_ad === '0') {
             $statistics->where('is_ad', $request->is_ad);
