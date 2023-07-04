d36612879271fc3d461d416de981d845a4de36cf
Fedorov Nikita <fedorov.nukuta@gmail.com>
2023-07-04 12:06:09 +0300

    Add request for store statistic method

--- app/Http/Requests/Statistic/StoreRequest.php
+++ app/Http/Requests/Statistic/StoreRequest.php
@@ -13,7 +13,7 @@ class StoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class StoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'storage_music' => ['required', 'string', 'min:1', 'max:255'],
         ];
     }
 }
