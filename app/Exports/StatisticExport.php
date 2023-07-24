aec6cf6b8aa05c98b851d17e0b48ca22fc136e62
Fedorov Nikita <fedorov.nukuta@gmail.com>
2023-07-24 16:39:22 +0300

    Change export statistics data

--- app/Exports/StatisticExport.php
+++ app/Exports/StatisticExport.php
@@ -6,6 +6,7 @@ use App\Models\Ad;
 use App\Models\Statistic;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
@@ -23,7 +24,9 @@ class StatisticExport implements FromCollection, WithMapping, WithHeadings
     {
         return [
             '#',
-            'URL музыки/рекламы',
+            'Стиль музыки',
+            'Подстиль музыки',
+            'Название',
             'Музыка/Реклама',
             'Дата добавления',
         ];
@@ -33,7 +36,9 @@ class StatisticExport implements FromCollection, WithMapping, WithHeadings
     {
         return [
             $invoice->id,
-            $invoice->storage_music,
+            $invoice->style,
+            $invoice->substyle,
+            $invoice->music,
             $invoice->is_ad ? 'Реклама' : 'Музыка',
             date('d.m.Y H:i:s', strtotime($invoice->created_at))
         ];
@@ -55,8 +60,19 @@ class StatisticExport implements FromCollection, WithMapping, WithHeadings
                 $name = Ad::where('storage', 'LIKE', '%' . end($temp_arr))->where('user_id', $temp_arr[count($temp_arr) - 2])->first();
 
                 if ($name) {
-                    $statistics[$key]['storage_music'] = $name->name;
+                    $statistics[$key]['music'] = $name->name;
                 }
+            } else {
+                $temp_arr = explode('/', str_replace(['/storage/music/', 'music/'], ['', ''], $item['storage_music']));
+
+                foreach ($temp_arr as $key_music => $item_music) {
+                    switch ($key_music) {
+                        case 0: $statistics[$key]['style'] = (Storage::exists('public/music/' . $item_music . '/config.json') ? json_decode(Storage::get('public/music/' . $item_music . '/config.json'))->name : 'Данные не найдены'); break;
+                        case 1: $statistics[$key]['substyle'] = (Storage::exists('public/music/' . $temp_arr[$key_music - 1] . '/' . $item_music . '/config.json') ? json_decode(Storage::get('public/music/' . $temp_arr[$key_music - 1] . '/' . $item_music . '/config.json'))->name : 'Данные не найдены'); break;
+                    }
+                }
+
+                $statistics[$key]['music'] = urldecode(last($temp_arr));
             }
         }
 
